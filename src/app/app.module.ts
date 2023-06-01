bd501b1c243f6f0ebb4475b66e85be620361589d
vitor silva <vitorsilvadeoliveira050@gmail.com>
2023-06-01 20:17:21 -0300

    Front

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { SharedModule } from './shared/shared.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './template/header/header.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { HomeComponent } from './template/home/home.component';
+import { ListarProdutosComponent } from './components/produto/listar-produtos/listar-produtos.component';
+import { CadastrarProdutoComponent } from './components/produto/listar-produtos/cadastrar-produto/cadastrar-produto.component';
+
+import { LOCALE_ID } from '@angular/core';
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData} from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule, HttpContext} from '@angular/common/http';
+
+registerLocaleData(localePt);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ListarProdutosComponent,
+    CadastrarProdutoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    SharedModule,
+    HttpClientModule
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR'}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
